2b487e9acc3b0faa30841e58d0a69148dc2bb65b
ekgame <ekgame1@gmail.com>
2022-12-01 20:17:09 +0200

    Completed day 1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .fold(mutableListOf<MutableList<Int>>(mutableListOf())) { acc, s ->
+                if (s.isBlank()) {
+                    acc.add(mutableListOf())
+                } else {
+                    acc.last().add(s.toInt())
+                }
+                acc
+            }.maxOfOrNull { it.sum() } ?: 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .fold(mutableListOf<MutableList<Int>>(mutableListOf())) { acc, s ->
+                if (s.isBlank()) {
+                    acc.add(mutableListOf())
+                } else {
+                    acc.last().add(s.toInt())
+                }
+                acc
+            }.map { it.sum() }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("01.test")
+    check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = readInput("01")
     println(part1(input))
     println(part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,16 +1,6 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
-
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
+fun readInput(name: String) = File("inputs", "$name.txt")
     .readLines()
-
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
